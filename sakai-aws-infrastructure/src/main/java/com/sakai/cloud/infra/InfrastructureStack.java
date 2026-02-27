92f77b1d029236a07372a522ef079ded1e0bb618
jre <yuri.rempelj@gmail.com>
2026-02-27 09:11:18 +0100

    refs. #5: Refactoring `InfrastructureStack`.

--- sakai-aws-infrastructure/src/main/java/com/sakai/cloud/infra/InfrastructureStack.java
+++ sakai-aws-infrastructure/src/main/java/com/sakai/cloud/infra/InfrastructureStack.java
@@ -21,8 +21,8 @@ public class InfrastructureStack extends Stack {
     private final String ARTIFACT_OBJECT_KEY;
     private final Table INVENTORY_TABLE;
 
-    public InfrastructureStack(Construct scope, String id, StackProps props) {
-        super(scope, id, props);
+    public InfrastructureStack(Construct app, String id, StackProps props) {
+        super(app, id, props);
 
         String stage = (String) this.getNode().tryGetContext("stage");
 
@@ -31,39 +31,18 @@ public class InfrastructureStack extends Stack {
         ARTIFACT_OBJECT_KEY = (String) this.getNode().tryGetContext("artifactObjectKey");
         if (ARTIFACT_OBJECT_KEY == null) throw new RuntimeException("Artifact Object Key is not defined");
 
-        // DynamoDB Table
+        // === DynamoDB Table ===
         INVENTORY_TABLE = createDynamoDbTable(stage);
 
         // S3 Artifact Bucket
         IBucket artifactBucket = Bucket.fromBucketName(this, "ArtifactBucketId", ARTIFACT_BUCKET_NAME);
 
-        // Lambda Function Role
-        RoleProps lambdaRoleProps = RoleProps.builder()
-                .assumedBy(new ServicePrincipal("lambda.amazonaws.com"))
-                .managedPolicies(List.of(
-                        ManagedPolicy.fromAwsManagedPolicyName("service-role/AWSLambdaBasicExecutionRole")
-                ))
-                .build();
-
-        Role lambdaExecRole = new Role(this, "LambdaExecutionRoleId", lambdaRoleProps);
+        // === Lambda Function Role ===
+        Role lambdaExecRole = createLambdaExecRole();
         INVENTORY_TABLE.grantReadData(lambdaExecRole);
 
         // === Function for ListArticles Handler ===
-        Map<String, String> envVars = new HashMap<>();
-        envVars.put("TABLE_NAME", INVENTORY_TABLE.getTableName());
-
-        FunctionProps laFuncProps = FunctionProps.builder()
-                .runtime(Runtime.JAVA_21)
-                .memorySize(1024)
-                .handler("com.sakai.inventory.api.handler.ListArticlesHandler::handleRequest")
-                .architecture(Architecture.X86_64)
-                .timeout(Duration.seconds(30))
-                .environment(envVars)
-                .role(lambdaExecRole)
-                .code(Code.fromBucket(artifactBucket, ARTIFACT_OBJECT_KEY))
-                .build();
-
-        Function listArticlesHandler = new Function(this, "ListArticlesHandlerFunction", laFuncProps);
+        Function listArticlesHandler = createLambdaFunction(lambdaExecRole, artifactBucket);
     }
 
     private Table createDynamoDbTable(String stage) {
@@ -85,6 +64,35 @@ public class InfrastructureStack extends Stack {
                 .build();
 
         return new Table(this, "InventoryTableId", inventoryTableProps);
+    }
+
+    private Role createLambdaExecRole() {
+        RoleProps lambdaRoleProps = RoleProps.builder()
+                .assumedBy(new ServicePrincipal("lambda.amazonaws.com"))
+                .managedPolicies(List.of(
+                        ManagedPolicy.fromAwsManagedPolicyName("service-role/AWSLambdaBasicExecutionRole")
+                ))
+                .build();
+
+        return new Role(this, "LambdaExecutionRoleId", lambdaRoleProps);
+    }
+
+    private Function createLambdaFunction(Role lambdaExecRole, IBucket artifactBucket) {
+        Map<String, String> envVars = new HashMap<>();
+        envVars.put("TABLE_NAME", INVENTORY_TABLE.getTableName());
+
+        FunctionProps laFuncProps = FunctionProps.builder()
+                .runtime(Runtime.JAVA_21)
+                .memorySize(1024)
+                .handler("com.sakai.inventory.api.handler.ListArticlesHandler::handleRequest")
+                .architecture(Architecture.X86_64)
+                .timeout(Duration.seconds(30))
+                .environment(envVars)
+                .role(lambdaExecRole)
+                .code(Code.fromBucket(artifactBucket, ARTIFACT_OBJECT_KEY))
+                .build();
+
+        return new Function(this, "ListArticlesHandlerFunction", laFuncProps);
     }
 
     private RemovalPolicy determinateRemovalPolicy(String stage) {
