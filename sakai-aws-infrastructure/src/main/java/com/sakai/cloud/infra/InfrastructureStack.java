0e4d5391404da619235de847f6dc3d1fe03c3e44
jre <yuri.rempelj@gmail.com>
2026-02-27 16:18:14 +0100

    CloudWatch Role für `RestApi` erlaubt.

--- sakai-aws-infrastructure/src/main/java/com/sakai/cloud/infra/InfrastructureStack.java
+++ sakai-aws-infrastructure/src/main/java/com/sakai/cloud/infra/InfrastructureStack.java
@@ -6,8 +6,10 @@ import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
 import software.amazon.awscdk.services.apigateway.*;
 import software.amazon.awscdk.services.dynamodb.*;
-import software.amazon.awscdk.services.events.targets.ApiGatewayProps;
-import software.amazon.awscdk.services.iam.*;
+import software.amazon.awscdk.services.iam.ManagedPolicy;
+import software.amazon.awscdk.services.iam.Role;
+import software.amazon.awscdk.services.iam.RoleProps;
+import software.amazon.awscdk.services.iam.ServicePrincipal;
 import software.amazon.awscdk.services.lambda.*;
 import software.amazon.awscdk.services.lambda.Runtime;
 import software.amazon.awscdk.services.s3.Bucket;
@@ -116,6 +118,7 @@ public class InfrastructureStack extends Stack {
                 .description("API for Inventory Management System")
                 .deployOptions(deployOpt)
                 .defaultCorsPreflightOptions(corsOpt)
+                .cloudWatchRole(true)
                 .build();
 
         RestApi restApi = new RestApi(this, "RestApiGateway", props);
