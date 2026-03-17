451c922fed3e14e3715a8b375eb4396479ef1cd4
jre <yuri.rempelj@gmail.com>
2026-03-17 09:25:57 +0100

    refs #BE-09: Erweiterungen der Klass `PipelineApp` um Umgebungsvariablen. Berechtigungen und Stack-Provisionierung in `BackendPipelineStack` überarbeitet, einschließlich neuer Pipeline-Rolle. Aktualisierung der `pom.xml`-Build-Konfiguration.

--- backend-cicd-pipeline/src/main/java/com/sakai/cloud/infra/pipeline/BackendPipelineStack.java
+++ backend-cicd-pipeline/src/main/java/com/sakai/cloud/infra/pipeline/BackendPipelineStack.java
@@ -25,7 +25,7 @@ public class BackendPipelineStack extends Stack {
     private final String connectionId = "arn:aws:codeconnections:eu-central-1:315735600242:connection/5b463871-e022-42cc-831b-be409b55e94b";
 
     public BackendPipelineStack(Construct app, String id, StackProps props) {
-        super(app, "PipelineStackId", props);
+        super(app, id, props);
 
         // Konfiguration aus Context oder Umgebungsvariablen
         String repoOwner = (String) this.getNode().tryGetContext("repoOwner");
@@ -53,7 +53,7 @@ public class BackendPipelineStack extends Stack {
                 .computeType(ComputeType.SMALL)
                 .build();
 
-        BuildSpec buildSpecFile = BuildSpec.fromSourceFilename("buildspec.yaml");
+        BuildSpec buildSpecFile = BuildSpec.fromSourceFilename("sakai-aws-infrastructure/buildspec.yaml");
 
         PipelineProjectProps pipeLineProjectProps = PipelineProjectProps.builder()
                 .environment(buildEnvironment)
@@ -129,36 +129,16 @@ public class BackendPipelineStack extends Stack {
                 .resources(List.of(connectionId))
                 .build());
 
-        PolicyStatement cdkDeployPolicyStatement = PolicyStatement.Builder.create()
+        // === Pipeline Role ===
+        Role pipelineRole = new Role(this, "PipelineRole", RoleProps.builder()
+                .assumedBy(new ServicePrincipal("codepipeline.amazonaws.com"))
+                .build());
+
+        pipelineRole.addToPolicy(PolicyStatement.Builder.create()
                 .effect(Effect.ALLOW)
-                .actions(List.of(
-                        "cloudformation:CreateStack",
-                        "cloudformation:UpdateStack",
-                        "cloudformation:DescribeStacks",
-                        "cloudformation:DescribeStackEvents",
-                        "cloudformation:GetTemplate",
-
-                        "cloudformation:GetTemplateSummary",
-                        "cloudformation:DeleteStack",
-                        "cloudformation:ValidateTemplate"))
-                .resources(List.of(
-                        "arn:aws:cloudformation:*:*:stack/InfrastructureStack",
-                        "arn:aws:cloudformation:*:*:stack/InfrastructureStack/*"))
-                .build();
-
-        PolicyStatement iamLimitedPolicy = PolicyStatement.Builder.create()
-                .effect(Effect.ALLOW)
-                .actions(List.of(
-                        "iam:GetRole",
-                        "iam:CreateRole",
-                        "iam:PutRolePolicy",
-                        "iam:AttachRolePolicy",
-                        "iam:PassRole"
-                ))
-                .resources(List.of("arn:aws:iam::*:role/InfrastructureStack"))
-                .build();
-
-        pipelineProject.getRole().addToPrincipalPolicy(cdkDeployPolicyStatement);
+                .actions(List.of("codestar-connections:UseConnection"))
+                .resources(List.of(connectionId))
+                .build());
 
         // === Pipeline ===
         StageProps sourceStage = StageProps.builder()
@@ -187,7 +167,7 @@ public class BackendPipelineStack extends Stack {
                 .build();
 
         PipelineProps pipelineProps = PipelineProps.builder()
-                .role(pipelineProjectRole)
+                .role(pipelineRole)
                 .stages(List.of(sourceStage, buildStage))
                 .build();
 
